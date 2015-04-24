860b6553b9f7f0eba8607417314916f850ec1438
Tim Fletcher <tim@night-shade.org.uk>
2015-04-24 22:00:23 +0100

    Reindent correctly

--- examples/lvm-to-zfs-sync.sh
+++ examples/lvm-to-zfs-sync.sh
@@ -26,44 +26,44 @@ blocksize=8192
 keep=7
 
 if [ $(id -u) -gt 0 ] ; then
-        echo $0 needs to be run as root
-        exit 1
+    echo $0 needs to be run as root
+    exit 1
 fi
 
 baseline()
 {
-size=$(blockdev --getsize64 /dev/zvol/$localtarget/$dev)
-bdsync --diffsize --blocksize=$blocksize --remdata "ssh $remotehost bdsync --server" /dev/null $remotetarget/$dev | pv -s $size | bdsync --patch=/dev/zvol/$localtarget/$dev
+    size=$(blockdev --getsize64 /dev/zvol/$localtarget/$dev)
+    bdsync --diffsize --blocksize=$blocksize --remdata "ssh $remotehost bdsync --server" /dev/null $remotetarget/$dev | pv -s $size | bdsync --patch=/dev/zvol/$localtarget/$dev
 }
 
 devcheck()
 {
-if [ ! -b /dev/zvol/$localtarget/$dev ] ; then
-	echo "$localtarget/$dev doesn't exist"
-	exit 1
-fi
+    if [ ! -b /dev/zvol/$localtarget/$dev ] ; then
+        echo "$localtarget/$dev doesn't exist"
+        exit 1
+    fi
 }
 
 snapshot()
 {
-zfs-auto-snapshot.sh --syslog -p snap --label=bdsync --keep=$keep $localtarget/$dev
+    zfs-auto-snapshot.sh --syslog -p snap --label=bdsync --keep=$keep $localtarget/$dev
 }
 transfer()
 {
-bdsync --blocksize=$blocksize --remdata "ssh $remotehost bdsync --server" /dev/zvol/$localtarget/$dev $remotetarget/$dev | pv | sudo bdsync --patch
+    bdsync --blocksize=$blocksize --remdata "ssh $remotehost bdsync --server" /dev/zvol/$localtarget/$dev $remotetarget/$dev | pv | sudo bdsync --patch
 }
 
 case $1 in
-baseline)
+    baseline)
         shift
-	dev=$1
-	devcheck
-	baseline
-;;
-*)
-	dev=$1
-	devcheck
-	snapshot
-	transfer
-;;
+        dev=$1
+        devcheck
+        baseline
+        ;;
+    *)
+        dev=$1
+        devcheck
+        snapshot
+        transfer
+        ;;
 esac
