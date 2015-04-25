dd21a05dc8f6905eda566818dd9e4058cf4ba8e0
Tim Fletcher <tim@night-shade.org.uk>
2015-04-25 10:39:43 +0100

    Change to use /usr/bin/time in test9 for more details

--- tests/test9.sh
+++ tests/test9.sh
@@ -16,13 +16,13 @@ do_check ()
     truncate -s $SIZE $REMDEV 
 
     echo "no --zeroblock set"
-    time ./bdsync --remdata "./bdsync -s" $LOCDEV $REMDEV > /dev/null || abort_msg "bdsync (1) failed"
+    /usr/bin/time ./bdsync --remdata "./bdsync -s" $LOCDEV $REMDEV > /dev/null || abort_msg "bdsync (1) failed"
     echo "--zeroblock set on remote"
-    time ./bdsync --remdata "./bdsync -s --zeroblock" $LOCDEV $REMDEV > /dev/null || abort_msg "bdsync (2) failed"
+    /usr/bin/time ./bdsync --remdata "./bdsync -s --zeroblock" $LOCDEV $REMDEV > /dev/null || abort_msg "bdsync (2) failed"
     echo "--zeroblock set on local"
-    time ./bdsync --remdata --zeroblock "./bdsync -s" $LOCDEV $REMDEV > /dev/null || abort_msg "bdsync (3) failed"
+    /usr/bin/time ./bdsync --remdata --zeroblock "./bdsync -s" $LOCDEV $REMDEV > /dev/null || abort_msg "bdsync (3) failed"
     echo "--zeroblock set on both"
-    time ./bdsync --remdata --zeroblock "./bdsync -s --zeroblock" $LOCDEV $REMDEV > /dev/null || abort_msg "bdsync (4) failed"
+    /usr/bin/time ./bdsync --remdata --zeroblock "./bdsync -s --zeroblock" $LOCDEV $REMDEV > /dev/null || abort_msg "bdsync (4) failed"
 }
 
 handle_check do_check "--zeroblock speed test"
